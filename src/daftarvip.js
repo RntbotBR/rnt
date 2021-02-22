34d89c3deec26adb8e7a887221667a18218bbf72
RntbotBR <78214269+RntbotBR@users.noreply.github.com>
2021-02-22 15:29:10 -0300

    Add files via upload

---
+++ src/daftarvip.js
@@ -0,0 +1,17 @@
+const daftarvip = (prefix) => { 
+	return `
+
+*SE QUER REGISTAR VIP :*
+
+*Proprietário do bate-papo BOT :*
+
+_wa.me/558897966858 ou digite *${prefix}owner*_
+
+*NOTA*
+
+*GRUPO DO 🔥☠︎︎𝗥⃟𝗡𝗧BOT☠︎︎͜͡🇧🇷🔥 :*
+*╔═══❖•ೋ° 🔥 °ೋ•❖═══╗*
+https://chat.whatsapp.com/CwRKJoTB5Vt9NOTTsZwAw3
+*╚═══❖•ೋ° 🔥 °ೋ•❖═══╝* `
+}
+exports.daftarvip = daftarvip
---
+++ src/help1.js
@@ -0,0 +1,10 @@
+const help1 = (prefix) => {
+	return `
+┃MENU EM ATUALIZAÇÃO ⚠️
+┣QUALQUER COISA
+┣CHAME O DONO
+┣wa.me/+558897966858`
+}
+exports.help1 = help1
+
+
